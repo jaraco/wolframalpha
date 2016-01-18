5fc802f6e7a3b50074f04c37990ebaf6a7fec963
Jason R. Coombs <jaraco@jaraco.com>
2016-01-18 16:07:31 -0500

    Remove hard-coded API key from the library.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,12 @@
+2.0
+===
+
+``pmxbot`` plugin now requires that the "Wolfram|Alpha API key"
+config parameter be supplied. The hard-coded key has been
+removed and will be de-activated. Users must register for their
+own key at the `Wolfram|Alpha developer web site
+<https://developer.wolframalpha.com>`_.
+
 1.4
 ===
 
--- wolframalpha/pmxbot.py
+++ wolframalpha/pmxbot.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import pmxbot
 from pmxbot.core import command
 
 import wolframalpha
 
 @command("wolframalpha", aliases=('wa',), doc="Wolfram Alpha rules")
 def wa(client, event, channel, nick, rest):
-	client = wolframalpha.Client('Q59EW4-UEL27J79UK')
+	client = wolframalpha.Client(pmxbot.config['Wolfram|Alpha API key'])
 	res = client.query(rest)
 	return next(res.results).text
